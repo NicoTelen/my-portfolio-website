65b8b0cfaa5591e459ebd94dbd77d383ed2772ba
NicoTelen <nicochristiantelen@gmail.com>
2026-05-08 09:24:14 +0800

    Modern portfolio upgrade

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-function showMessage() {
-    alert("Hello! Welcome to my website!");
-}
+// DARK MODE
+const darkModeBtn = document.getElementById("darkModeBtn");
+
+
+darkModeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+});
+
+
+// TYPING ANIMATION
+var typed = new Typed(".typing", {
+    strings: [
+        "Web Developer",
+        "Frontend Developer",
+        "System Developer",
+        "Student Programmer"
+    ],
+
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+
+// AOS SCROLL ANIMATION
+AOS.init({
+    duration: 1200,
+});
